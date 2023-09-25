5ffd3c608da3dc80e424b37a21ac1a9f7795e06a
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-09-24 23:09:31 -0400

    should return 2 for imput 2

--- src/primerFactor.js
+++ src/primerFactor.js
@@ -1,7 +1,12 @@
 class PrimerFactor {
 
+
   static generate(n){
-    return[];
+    const primes = [];
+    if(n>1){
+      primes.push(2);
+    }
+    return primes;
   }
 }
 
--- src/primerFactor.test.js
+++ src/primerFactor.test.js
@@ -1,7 +1,14 @@
 import PrimerFactor from "./primerFactor.js";
 
 describe("Primer factor", () => {
+  function list(...ints){
+    return ints;
+  }
   it("should return an empty array for input 1", () => {
     expect(PrimerFactor.generate(1)).toEqual([]);
+  });
+
+  it("should return[2] for input 2", () => {
+    expect(PrimerFactor.generate(2)).toEqual(list(2));
   });
 });
